e2ad72d17f07f6ddf926fa508984befc34cb589b
maritgh <maritghsnijder@gmail.com>
2024-05-28 11:49:13 +0200

    werkende printer code

--- printer/printer.ino
+++ printer/printer.ino
@@ -1,23 +1,38 @@
-#include "Adafruit_Thermal.h"
+#include <Adafruit_Thermal.h>
 #include "SoftwareSerial.h"
+//#include <AltSoftSerial.h>
 #include <DS3231.h>
  
 #define TX_PIN 6
 #define RX_PIN 5
- 
+
+//AltSoftSerial mySerial(RX_PIN, TX_PIN);
 SoftwareSerial mySerial(RX_PIN, TX_PIN);
 Adafruit_Thermal printer(&mySerial);
  
 void setup() {
-  pinMode(7, OUTPUT);
-  digitalWrite(7, LOW);
+  // pinMode(7, OUTPUT);
+  // digitalWrite(7, LOW);
  
   mySerial.begin(9600);
   Serial.begin(9600);
   printer.begin();
   printer.setHeatConfig(11, 200, 90);
-  delay(10000); // Give the printer some time to initialize
+ printer.sleep();
+}
  
+void loop() {
+  printbon();
+  delay(1000);
+}
+void printbon(){
+  printer.wake();
+
+  delay(1000); // Give the printer some time to initialize
+  printer.println("\n");
+  printer.justify('L');
+  printer.setLineHeight(30);
+
   // Print the header
   printer.doubleHeightOn();
   printer.println(F("RTB"));
@@ -27,24 +42,24 @@ void setup() {
   // Print date and time
   printer.justify('C');
   printer.println(F("Date"));
-  delay(500); // Increased delay
+  delay(200); // Increased delay
   printer.justify('C');
   printer.println(F("Time"));
-  delay(500); // Increased delay
+  delay(200); // Increased delay
  
   // Print pasnummer
   printer.justify('C');
   printer.println(F("Card"));
   printer.underlineOn();
-  delay(500);
+  delay(200);
   printer.justify('C');
   printer.println(F("Number:"));
   printer.underlineOn();
-  delay(500);
+  delay(200);
   printer.println(F("NL XXXX"));
   printer.println(F("XXXX XX XX"));
   printer.underlineOff();
-  delay(500); // Increased delay
+  delay(200); // Increased delay
  
   // Print gepind bedrag with smaller font size
   printer.setFont(1); // Set smaller font size
@@ -55,15 +70,13 @@ void setup() {
   printer.justify('C');
   printer.println(F("Amount"));
   printer.underlineOn();
-  delay(500);
+  delay(200);
  
   printer.println(F("100,00"));
   printer.underlineOff();
   printer.setFont(0); // Revert to the original font size
-  delay(500); // Increased delay
+  delay(200); // Increased delay
  
-  // Print thank you message
-  // Print thank you message
 printer.justify('C');
 printer.println(F("Thank\nyou"));
 printer.setLineHeight(50);
@@ -72,9 +85,7 @@ printer.println(F("Raccoon"));
  
 printer.justify('C');
 printer.println(F("Trust &\nBank"));
- 
-}
- 
-void loop() {
-  // Do nothing in the loop function
+printer.println("\n");
+printer.println("\n");
+printer.sleep();
 }
--- samenvoegen/samenvoegen.ino
+++ samenvoegen/samenvoegen.ino
@@ -6,10 +6,10 @@
 // #include "SoftwareSerial.h"
 // #include <DS3231.h>
 //rfid scanner
-#define RST_PIN         9  
-#define SS_PIN          10
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
- 
+#define RST_PIN 9
+#define SS_PIN 10
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
+
 MFRC522::MIFARE_Key key;
 MFRC522::StatusCode status;
 byte blockAddr4 = 4;
@@ -19,17 +19,17 @@ byte len = 18;
 char iban[36];
 char uid[12];
 String bon;
- 
+
 //bon printer
-// #define TX_PIN 6
-// #define RX_PIN 5
- 
+#define TX_PIN 6
+#define RX_PIN 5
+
 // SoftwareSerial mySerial(RX_PIN, TX_PIN);
 // Adafruit_Thermal printer(&mySerial);
- 
+
 //keypad
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
- 
+
 //Use this to get correct key output
 char KEYS[] = {
   'D', '3', '2', '1',
@@ -37,15 +37,15 @@ char KEYS[] = {
   'E', '9', '8', '7',
   'x', 'X', '0', 'x'
 };
- 
-OnewireKeypad <Print, 16> KP2(Serial, KEYS, 4, 4, A1, 4700, 1000);
- 
+
+OnewireKeypad<Print, 16> KP2(Serial, KEYS, 4, 4, A1, 4700, 1000);
+
 void setup() {
   lcd.begin(16, 2);
   Serial.begin(9600);
   KP2.SetKeypadVoltage(5.0);
-  SPI.begin();        // Init SPI bus
-  mfrc522.PCD_Init(); // Init MFRC522 card
+  SPI.begin();         // Init SPI bus
+  mfrc522.PCD_Init();  // Init MFRC522 card
   // Prepare the key (used both as key A and as key B)
   // using FFFFFFFFFFFFh which is the default at chip delivery from the factory
   for (byte i = 0; i < 6; i++) {
@@ -53,127 +53,122 @@ void setup() {
   }
   pinMode(7, OUTPUT);
   digitalWrite(7, LOW);
-  // printer.begin();
-  // delay(500);
-  // printer.sleep();
-       
+  printer.begin();
+  printer.setHeatConfig(11, 200, 90);
+  printer.sleep();
 }
- 
+
 void loop() {
- 
+
   cardscan();
- 
- 
+
+
   byte KState = KP2.Key_State();
   if (KState == PRESSED) {
     keypadpress();
   }
- 
+
   int value_key = analogRead(A2);
- 
+
   /*If two buttons are pressed at the time then lowest value is used*/
- 
+
   if (value_key <= 900) {
     buttonpress(value_key);
   }
-  if(Serial.read()== 'q'){
+  if (Serial.read() == 'q') {
     bon = Serial.readStringUntil('q');
     Serial.println(bon);
   }
- 
 }
- 
-void cardscan(){
+
+void cardscan() {
   mfrc522.PCD_Init();
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
+      key.keyByte[i] = 0xFF;
     }
-    if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()){
-      for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
-      }
-      return;
-    }
- 
-    printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
-    Serial.println();
- 
-    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockAddr4, &key, &(mfrc522.uid));
-    if (status != MFRC522::STATUS_OK) {
-        Serial.print(F("Authentication failed: ")); Serial.println(mfrc522.GetStatusCodeName(status));
-        for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
-        }
-        return;
-    }
-    status = mfrc522.MIFARE_Read(blockAddr4, buffer1, &len);
-    if (status != MFRC522::STATUS_OK) {
-      Serial.print(F("Reading failed: "));
-      Serial.println(mfrc522.GetStatusCodeName(status));
-      for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
-      }
-      return;
-    }
-    for (uint8_t i = 0; i < 16; i++)
-    {
-      if (buffer1[i] != 32)
-      {
-        iban[i]=buffer1[i];
-      }
-    }
-    Serial.print(" ");
-    memset(buffer1, 0, sizeof(buffer1));
-    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockAddr5, &key, &(mfrc522.uid));
-    if (status != MFRC522::STATUS_OK) {
-      Serial.print(F("Authentication failed: ")); Serial.println(mfrc522.GetStatusCodeName(status));
-      for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
-      }
-      return;
-    }
-    status = mfrc522.MIFARE_Read(blockAddr5, buffer1, &len);
-    if (status != MFRC522::STATUS_OK) {
-      Serial.print(F("Reading failed: "));
-      Serial.println(mfrc522.GetStatusCodeName(status));
-      for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
-      }
-      return;
-    }
-    for (uint8_t i = 0; i < 16; i++)
-    {
-      if (buffer1[i] != 32)
-      {
-        iban[i+8]=buffer1[i];
-      }
-    }
-    memset(buffer1, 0, sizeof(buffer1));
+    return;
+  }
+
+  printHex(mfrc522.uid.uidByte, mfrc522.uid.size);
+  Serial.println();
+
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockAddr4, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
     for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
+      key.keyByte[i] = 0xFF;
     }
- 
-    mfrc522.PICC_HaltA();
-    mfrc522.PCD_StopCrypto1();
-    Serial.println(iban);
-    Serial.println(uid);
-    mfrc522.PCD_Reset();
-    delay(100);
- 
+    return;
+  }
+  status = mfrc522.MIFARE_Read(blockAddr4, buffer1, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    for (byte i = 0; i < 6; i++) {
+      key.keyByte[i] = 0xFF;
+    }
+    return;
+  }
+  for (uint8_t i = 0; i < 16; i++) {
+    if (buffer1[i] != 32) {
+      iban[i] = buffer1[i];
+    }
+  }
+  Serial.print(" ");
+  memset(buffer1, 0, sizeof(buffer1));
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, blockAddr5, &key, &(mfrc522.uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    for (byte i = 0; i < 6; i++) {
+      key.keyByte[i] = 0xFF;
+    }
+    return;
+  }
+  status = mfrc522.MIFARE_Read(blockAddr5, buffer1, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    for (byte i = 0; i < 6; i++) {
+      key.keyByte[i] = 0xFF;
+    }
+    return;
+  }
+  for (uint8_t i = 0; i < 16; i++) {
+    if (buffer1[i] != 32) {
+      iban[i + 8] = buffer1[i];
+    }
+  }
+  memset(buffer1, 0, sizeof(buffer1));
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();
+  Serial.println(iban);
+  Serial.println(uid);
+  mfrc522.PCD_Reset();
+  delay(100);
 }
- 
-void keypadpress(){
+
+void keypadpress() {
   // Serial.print("keypad : ");
   char Key;
   if (Key = KP2.Getkey()) {
-      Serial.println(Key);
-      lcd.setCursor(0, 0);
-      lcd.clear();
-      lcd.print(Key);
-    }
+    Serial.println(Key);
+    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.print(Key);
+  }
 }
- 
-void buttonpress(int value_key){
+
+void buttonpress(int value_key) {
   // Serial.print("button : ");
   if (value_key > 850) {
     Serial.println("z");
@@ -187,12 +182,12 @@ void buttonpress(int value_key){
     Serial.println("v");
   } else if (value_key < 450) {
     Serial.println("u");
-  } else{
+  } else {
     Serial.println("Error");
   }
-   delay(500);
+  delay(500);
 }
- 
+
 void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
@@ -200,61 +195,67 @@ void printHex(byte *buffer, byte bufferSize) {
   }
 }
 
-// void printBon(){
-//   printer.wake();
-//   // Print the header
-//   printer.doubleHeightOn();
-//   printer.println(F("RTB"));
-//   printer.doubleHeightOff();
-//   delay(200); // Increased delay
-  
-//   // Print date and time
-//   printer.justify('C');
-//   printer.println(F("Date"));
-//   delay(200); // Increased delay
-//   printer.justify('C');
-//   printer.println(F("Time"));
-//   delay(200); // Increased delay
- 
-//   // Print pasnummer
-//   printer.justify('C');
-//   printer.println(F("Card"));
-//   printer.underlineOn();
-//   delay(200);
-//   printer.justify('C');
-//   printer.println(F("Number:"));
-//   printer.underlineOn();
-//   delay(200);
-//   printer.println(F("NL XXXX"));
-//   printer.println(F("XXXX XX XX"));
-//   printer.underlineOff();
-//   delay(200); // Increased delay
- 
-//   // Print gepind bedrag with smaller font size
-//   printer.setFont(1); // Set smaller font size
-//   printer.justify('C');
-//   printer.println(F("Pinned"));
-//   printer.underlineOn();
-//   printer.setFont(1); // Set smaller font size
-//   printer.justify('C');
-//   printer.println(F("Amount"));
-//   printer.underlineOn();
-//   delay(200);
- 
-//   printer.println(F("100,00"));
-//   printer.underlineOff();
-//   printer.setFont(0); // Revert to the original font size
-//   delay(200); // Increased delay
- 
-//   // Print thank you message
-//   // Print thank you message
-//   printer.justify('C');
-//   printer.println(F("Thank\nyou"));
-//   printer.setLineHeight(50);
-  
-//   printer.println(F("Raccoon"));
-  
-//   printer.justify('C');
-//   printer.println(F("Trust &\nBank"));
-//   printer.sleep();
-// }
+void printbon() {
+  printer.wake();
+
+  delay(1000);  // Give the printer some time to initialize
+  printer.println("\n");
+  printer.justify('L');
+  printer.setLineHeight(30);
+
+  // Print the header
+  printer.doubleHeightOn();
+  printer.println(F("RTB"));
+  printer.doubleHeightOff();
+  delay(500);  // Increased delay
+
+  // Print date and time
+  printer.justify('C');
+  printer.println(F("Date"));
+  delay(200);  // Increased delay
+  printer.justify('C');
+  printer.println(F("Time"));
+  delay(200);  // Increased delay
+
+  // Print pasnummer
+  printer.justify('C');
+  printer.println(F("Card"));
+  printer.underlineOn();
+  delay(200);
+  printer.justify('C');
+  printer.println(F("Number:"));
+  printer.underlineOn();
+  delay(200);
+  printer.println(F("NL XXXX"));
+  printer.println(F("XXXX XX XX"));
+  printer.underlineOff();
+  delay(200);  // Increased delay
+
+  // Print gepind bedrag with smaller font size
+  printer.setFont(1);  // Set smaller font size
+  printer.justify('C');
+  printer.println(F("Pinned"));
+  printer.underlineOn();
+  printer.setFont(1);  // Set smaller font size
+  printer.justify('C');
+  printer.println(F("Amount"));
+  printer.underlineOn();
+  delay(200);
+
+  printer.println(F("100,00"));
+  printer.underlineOff();
+  printer.setFont(0);  // Revert to the original font size
+  delay(200);          // Increased delay
+
+  printer.justify('C');
+  printer.println(F("Thank\nyou"));
+  printer.setLineHeight(50);
+
+  printer.println(F("Raccoon"));
+
+  printer.justify('C');
+  printer.println(F("Trust &\nBank"));
+  printer.println("\n");
+  printer.println("\n");
+  printer.sleep();
+}
